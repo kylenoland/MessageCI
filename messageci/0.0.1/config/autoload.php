b6129482e10e15f817c23b47b84b0cad2acf08e5
Kyle Noland <Kyle@lenovo-8e575d73.(none)>
2012-07-29 10:54:34 -0500

    Renamed spark to MessageCI to avoid collisions.

---
+++ messageci/0.0.1/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+# Load the Message config when the spark is loaded
+$autoload['config'] = array('messageci');
+
+# Load the Message library when the spark is loaded
+$autoload['libraries'] = array('messageci');
